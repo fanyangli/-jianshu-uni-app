bc960e60a4888be910cd33e4deade9c82b415977
Invader <1292689963@qq.com>
2019-03-28 09:48:17 +0800

    jianshu project

---
+++ pages/concern/concern.vue
@@ -0,0 +1,31 @@
+<template>
+	<view class="top">
+		
+	</view>
+	<view class="content">
+		
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+				messages:[
+					
+				]
+			}
+		},
+		onLoad() {
+
+		},
+		methods: {
+
+		}
+	}
+</script>
+
+<style>
+	
+	
+</style>
---
+++ pages/index/index.vue
@@ -0,0 +1,28 @@
+<template>
+	<view class="top">
+		
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+				messages:[
+					
+				]
+			}
+		},
+		onLoad() {
+
+		},
+		methods: {
+
+		}
+	}
+</script>
+
+<style>
+	
+	
+</style>
